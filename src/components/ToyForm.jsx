b73cdbb0b2fd6966465004cae79f43c9f65747de
denis <denisyegonn@gmail.com>
2026-09-09 09:41:03 +0300

    Updated

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,24 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ addToy }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const newToy = {
+      name: name,
+      image: image,
+      likes: 0,
+    };
+
+    fetch("http://localhost:6001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newToy),
+    })
+      .then((response) => response.json())
+      .then((toy) => {
+        addToy(toy);
+        setName("");
+        setImage("");
+      });
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
+
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
         />
+
         <br />
+
         <input
           type="text"
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={image}
+          onChange={(event) => setImage(event.target.value)}
         />
+
         <br />
+
         <input
           type="submit"
           name="submit"
