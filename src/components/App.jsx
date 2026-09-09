111d4393b4f3c4477c2daa136ca37135299ce75a
denis <denisyegonn@gmail.com>
2026-09-09 09:18:28 +0300

    Display -on page load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,17 @@ import React, { useState } from "react";
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
+import {useEffect, useState} from "react";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/toys")
+      .then((response) => response.json())
+      .then((data) => setToys(data));
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
